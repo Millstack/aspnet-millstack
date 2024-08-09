079ef718b56ac99103b433b84b244d7a4bab68cd
Milind Khamkar <fullstackwithmilind@gmail.com>
2024-08-10 00:35:45 +0530

    Common mastert two started

--- MILLSTACK/Master_Pages/CommonMasterTwo.aspx
+++ MILLSTACK/Master_Pages/CommonMasterTwo.aspx
@@ -2,11 +2,36 @@
 
 <%@ Register Namespace="AjaxControlToolkit" Assembly="AjaxControlToolkit" TagPrefix="ajax" %>
 
-<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
 </asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+<asp:Content ID="Content2" ContentPlaceHolderID="main" runat="Server">
 
 
+    <asp:Literal ID="Main_Column_1_Text" runat="server"></asp:Literal>
+    <asp:Literal ID="Main_Column_2_Text" runat="server"></asp:Literal>
+    <asp:Literal ID="Main_Column_3_Text" runat="server"></asp:Literal>
+    <asp:Literal ID="Foreign_Key_Text" runat="server"></asp:Literal>
+
+
+    <!-- Heading -->
+    <div class="col-md-11 mx-auto fw-normal fs-3 fw-bold ps-0 pb-2 text-dark-emphasis mt-1 mb-1 text-center">
+        <asp:Literal ID="Page_Heading" Text="" runat="server"></asp:Literal>
+    </div>
+
+    <!-- UI Starts -->
+    <div class="card col-md-11 mx-auto mt-1 py-2 shadow rounded-3">
+        <div class="card-body">
+
+            <!-- Heading - BG -->
+            <div class="fs-5 fw-medium text-white border border-dark-subtle bg-primary shadow rounded-2 text-center py-2 mb-3">
+                <asp:Literal Text="" runat="server"></asp:Literal>
+            </div>
+
+
+
+        </div>
+    </div>
+    <!-- UI Ends -->
 
 </asp:Content>
 
--- MILLSTACK/Master_Pages/CommonMasterTwo.aspx.cs
+++ MILLSTACK/Master_Pages/CommonMasterTwo.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommonClassLibrary;
+using System;
+using System.Activities.Expressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +9,73 @@ using System.Web.UI.WebControls;
 
 public partial class Master_Pages_CommonMasterTwo : System.Web.UI.Page
 {
+    #region [GLobal Declaration]
+    ExecuteClass executeClass = new ExecuteClass();
+    MasterClass masterClas = new MasterClass();
+
+    String sqlQuery = string.Empty;
+
+    static string
+        Main_Table_Name = "", Primary_Key_Column = "", Main_Column_1_Name = "", Main_Column_2_Name = "", Main_Column_3_Name = "",
+        Foreign_Table_Name = "", Foreign_Key_Column = "", Foreign_Column_Name = "";
+
+    #endregion
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        try
+        {
+            if (IsPostBack)
+            {
+                if (Request.QueryString.Count != 0)
+                {
+                    Decide_Page();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
 
+        }
     }
+
+
+    //================================ Page Type ========================================
+
+    private void Decide_Page()
+    {
+        if (Request.QueryString["P"].ToString().Trim() == "Division")
+        {
+            // current table details
+            Main_Table_Name = "Tbl_M_Division";
+            Primary_Key_Column = "DivisionID";
+
+            Main_Column_1_Text.Text = "Division";
+            Main_Column_1_Name = "Divisions";
+
+            Main_Column_2_Text.Text = "Division (Marathi)";
+            Main_Column_2_Name = "Divisions";
+
+            Main_Column_3_Text.Text = "Division Code";
+            Main_Column_3_Name = "DivisionCode";
+
+            // foriegn table details
+            Foreign_Table_Name = "Tbl_M_State";
+            Foreign_Column_Name = "StateName";
+
+            Foreign_Key_Text.Text = "State";
+            Foreign_Key_Column = "State_ID";
+
+            // gridview column names
+            Grid_Common.Columns[0].HeaderText = "Division ID";
+            Grid_Common.Columns[1].HeaderText = "Division";
+            Grid_Common.Columns[2].HeaderText = "Division (Marathi)";
+            Grid_Common.Columns[3].HeaderText = "Division Code";
+            Grid_Common.Columns[4].HeaderText = "State";
+
+            // to hode 2nd input column
+            Grid_Common.Columns[2].Visible = true;
+        }
+    }
+
 }
