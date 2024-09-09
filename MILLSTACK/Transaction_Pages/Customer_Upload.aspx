468bea321c0a2b2c645fa226a2bad3bbb24e67f7
Milind Khamkar <fullstackwithmilind@gmail.com>
2024-09-09 09:00:16 +0530

    customer excel data upload in progress

---
+++ MILLSTACK/Transaction_Pages/Customer_Upload.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="" Language="C#" Async="true" UnobtrusiveValidationMode="None" MaintainScrollPositionOnPostback="true" MasterPageFile="~/Admin/AdminMaster.master" AutoEventWireup="true" CodeFile="Customer_Upload.aspx.cs" Inherits="Transaction_Pages_Customer_Upload" %>
+
+<%@ Register Namespace="AjaxControlToolkit" Assembly="AjaxControlToolkit" TagPrefix="ajax" %>
+<%@ Register Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" TagPrefix="telerik" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="main" Runat="Server">
+
+
+
+
+
+
+</asp:Content>
+
---
+++ MILLSTACK/Transaction_Pages/Customer_Upload.aspx.cs
@@ -0,0 +1,34 @@
+﻿using CommonClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Transaction_Pages_Customer_Upload : System.Web.UI.Page
+{
+    #region [ Global Declaration ]
+    ExecuteClass executeClass = new ExecuteClass();
+    MasterClass masterClass = new MasterClass();
+    Dictionary<string, object> parameters = new Dictionary<string, object>();
+    #endregion
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!IsPostBack)
+            {
+
+            }
+        }
+        catch (Exception ex)
+        {
+            SweetAlert.GetSweet(this.Page, "error", "", $"An error occured: {ex.Message}");
+        }
+    }
+
+
+
+}
